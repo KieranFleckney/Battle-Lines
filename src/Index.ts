9e47eaf6f3edcd051cd64f4e242eebb2d8c06d0f
Kieran Fleckney <kieran.fleckney@hotmail.co.uk>
2020-06-20 22:43:25 +0100

    fix: Index missing a few exports

--- src/Index.ts
+++ src/Index.ts
@@ -1,9 +1,16 @@
+export { IRLG, New } from './IRLG';
 export { Config } from './Config';
+
+export { IMode } from './Modes/IMode';
+export { ClashMode } from './Modes/ClashMode';
+export { BattleField } from './Modes/BattleField';
+
+export { IRenderer } from './Renderers/IRenderer';
 export { CanvasRenderer } from './Renderers/CanvasRenderer';
 export { TextRenderer } from './Renderers/TextRenderer';
-export { ClashMode } from './Modes/ClashMode';
-export { IRLG, New } from './IRLG';
-export { IRenderer } from './Renderers/IRenderer';
+
+export { Grid } from './Grid/Grid';
 export { Cell } from './Grid/Cell';
+export { CellTypes } from './Grid/Cell-Types';
 
 export * from './Utilities';
